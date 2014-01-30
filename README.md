3db4291d6b3f201fe6808c7a59c9de4357a11a1b
John Otander <johnotander@gmail.com>
2014-01-30 11:31:16 -0700

    Update index for users.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Decorators on Rails
+
+An example that uses the Draper gem for Rails Decorators.
+
+
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.all.decorate
   end
 
   # GET /users/1
